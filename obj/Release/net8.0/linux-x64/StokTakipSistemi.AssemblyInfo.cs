6b644023e3579f642a591c1f9699fc1d4ea59456
deniizeynep <dnzzeynep7@gmail.com>
2026-05-29 00:50:47 +0300

    update code

--- obj/Release/net8.0/linux-x64/StokTakipSistemi.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/StokTakipSistemi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StokTakipSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217eada632e7d0bcbeaa603f5545aa9f817b3979")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9185fdd1dd79e1a361cfc7a5906aeb2c459c57")]
 [assembly: System.Reflection.AssemblyProductAttribute("StokTakipSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StokTakipSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
